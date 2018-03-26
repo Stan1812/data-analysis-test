4782b8b3e85c71a9bb21cfc3e9f1f687c93d2ebd
stan1812 <2314980309@qq.com>
2018-03-26 19:58:29 +0800

    basic feature

---
+++ index.js
@@ -0,0 +1,39 @@
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/";
+const log = console.log
+
+MongoClient.connect(url, (err, db) => {
+    if (err) throw err;
+    const arts = db.db("articles").collection("arts")
+    const authors = db.db("articles").collection("authors")
+    authors.ensureIndex()
+    let queryStr = {
+        "data.author": '王小波'
+    }
+    arts.distinct('data.author', (err, res) => {
+        if (err) console.log(err);
+        log(res)
+        authors.ensureIndex({
+            _id: 1
+        })
+        res.forEach(element => {
+            let newObj = {
+                'author': element,
+                'titles': []
+            }
+            authors.insertOne(newObj, (err, res) => {
+                if (err) throw err;
+                console.log(`${newObj}插入成功`);
+            });
+        })
+    });
+    // arts.find({"data.author":"王小波"}).toArray((err, result) => {
+    //     if (err) throw err;
+    //     console.log(result);
+    //     // let authorList =[]
+    //     // result.forEach(element => {
+    //     // authorList.push(element.author)            
+    //     // });
+    //     db.close();
+    // });
+});
