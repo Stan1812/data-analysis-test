8e8030112c230b88f9f59e4f690595d021544e74
stan1812 <2314980309@qq.com>
2018-03-27 00:46:27 +0800

    encountered some problem

--- src/main.js
+++ src/main.js
@@ -1,25 +1,24 @@
 // 引入 ECharts 主模块
-var echarts = require('echarts/lib/echarts');
-// 引入柱状图
+import {authors} from '../lib/author';
+const echarts = require('echarts/lib/echarts');
 require('echarts/lib/chart/bar');
-// 引入提示框和标题组件
 require('echarts/lib/component/tooltip');
 require('echarts/lib/component/title');
-// 基于准备好的dom，初始化echarts实例                                                                                                                                                 
-var myChart = echarts.init(document.getElementById('main'));                        
-// 绘制图表
+let myChart = echarts.init(document.getElementById('main'));
 myChart.setOption({
     title: {
-        text: 'ECharts 入门示例'
+        text: 'test'
     },
     tooltip: {},
     xAxis: {
-        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+        data: authors
     },
     yAxis: {},
-    series: [{
-        name: '销量',
-        type: 'bar',
-        data: [5, 20, 36, 10, 10, 20]
-    }]
+    series: [
+        {
+            name: '文章数',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20,2,2,2,22]
+        }
+    ]
 });
